554a0d70e1d30fe892b360cd355a0c4b172e0723
Gantzyo <gantzyo@gmail.com>
2016-10-08 12:05:39 +0200

    Steamgifts scripts v1.0.3
    - Removed .meta.js files (not neede for little scripts like these)
    - Change GM_getResourceURL to GM_getResourceText (Tag was wrong)

--- Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
+++ Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
@@ -3,7 +3,6 @@
 // @namespace       https://www.steamgifts.com/
 // @homepageURL     https://github.com/Gantzyo/Miniscripts/tree/master/Greasemonkey/Steamgifts/Steamgifts_auto_join
 // @supportURL      https://github.com/Gantzyo/Miniscripts/issues
-// @updateURL       https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.meta.js
 // @downloadURL     https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
 // @include         https://www.steamgifts.com/giveaway/*
 // @version         1.0.1
