c4bf741862dbb69848a118e8f37d7f55ee5815ee
Gantzyo <gantzyo@gmail.com>
2016-10-08 11:51:31 +0200

    Steamgifts scripts v1.0.2
    Añadido @grant GM_getResourceURL para poder cargar estilos

--- Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
+++ Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
@@ -6,8 +6,9 @@
 // @updateURL       https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.meta.js
 // @downloadURL     https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
 // @include         https://www.steamgifts.com/*
-// @version         1.0.1
+// @version         1.0.2
 // @grant           GM_addStyle
+// @grant           GM_getResourceURL
 // @require         https://code.jquery.com/jquery-3.1.1.min.js
 // @resource        styles https://raw.githubusercontent.com/Gantzyo/Miniscripts/master/Greasemonkey/Steamgifts/Steamgifts_points_checker/resources/styles.css
 // ==/UserScript==
