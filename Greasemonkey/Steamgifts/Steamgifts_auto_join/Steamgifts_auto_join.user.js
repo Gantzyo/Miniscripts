e7b86580e17485e0992a7c34c35438765c82107e
Gantzyo <gantzyo@gmail.com>
2016-10-08 15:32:24 +0200

    Steamgifts Scripts - Ejecución del script una vez ha cargado la página
    Añadido $(document).ready a todos los scripts para asegurarnos de que se
    ejecutan una vez cargada la web

--- Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
+++ Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
@@ -5,33 +5,15 @@
 // @supportURL      https://github.com/Gantzyo/Miniscripts/issues
 // @downloadURL     https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_auto_join/Steamgifts_auto_join.user.js
 // @include         https://www.steamgifts.com/giveaway/*
-// @version         1.0.4
+// @version         1.0.6
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @require         https://code.jquery.com/jquery-3.1.1.min.js
 // ==/UserScript==
 
-
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
- */
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// SCRIPT
-
-setTimeout(function() {
+$( document ).ready(function() {
+    // SCRIPT
     if(!$('.sidebar__entry-insert').hasClass('is-hidden')) {
      $('.sidebar__entry-insert').click();
     }
-}, getRandomInt(125, 250)); // Random delay before pressing the button
-
+});
--- Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
+++ Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
@@ -5,7 +5,7 @@
 // @supportURL      https://github.com/Gantzyo/Miniscripts/issues
 // @downloadURL     https://github.com/Gantzyo/Miniscripts/raw/master/Greasemonkey/Steamgifts/Steamgifts_points_checker/Steamgifts_points_checker.user.js
 // @include         https://www.steamgifts.com/*
-// @version         1.0.7
+// @version         1.0.8
 // @grant           GM_addStyle
 // @grant           GM_getResourceText
 // @grant           GM_openInTab
@@ -13,72 +13,73 @@
 // @resource        styles https://raw.githubusercontent.com/Gantzyo/Miniscripts/master/Greasemonkey/Steamgifts/Steamgifts_points_checker/resources/styles.css
 // ==/UserScript==
 
-
+$(document).ready(function () {
 // --- FUNCTIONS
-function cleanPointsString(string) {
-    return string.replace("(", "").replace("P)", "");
-}
+    function cleanPointsString(string) {
+        return string.replace("(", "").replace("P)", "");
+    }
 
-function checkJoinableGiveaways() {
-    $('div.giveaway__row-outer-wrap.SGPC_joinable').each(function () {
-        var $this = $(this);
-        var points = cleanPointsString($this.find('span.giveaway__heading__thin').last().html());
-        var totalpoints = $("span.nav__points").html();
-        if (totalpoints - points < 0) {
-            $this.removeClass('SGPC_joinable');
-            $this.addClass('SGPC_nonJoinable');
-        }
-    });
-}
+    function checkJoinableGiveaways() {
+        $('div.giveaway__row-outer-wrap.SGPC_joinable').each(function () {
+            var $this = $(this);
+            var points = cleanPointsString($this.find('span.giveaway__heading__thin').last().html());
+            var totalpoints = $("span.nav__points").html();
+            if (totalpoints - points < 0) {
+                $this.removeClass('SGPC_joinable');
+                $this.addClass('SGPC_nonJoinable');
+            }
+        });
+    }
 
 // --- STYLES
-GM_addStyle(GM_getResourceText("styles"));
+    GM_addStyle(GM_getResourceText("styles"));
 
 
 // --- SCRIPT
 
 // Add 'SGPC_joinable' class to all non-faded giveaways
-$('div.giveaway__row-inner-wrap:not(.is-faded)').each(function () {
-    var $this = $(this).parent(); // parent = div.giveaway__row-outer-wrap
-    $this.addClass('SGPC_joinable');
-});
+    $('div.giveaway__row-inner-wrap:not(.is-faded)').each(function () {
+        var $this = $(this).parent(); // parent = div.giveaway__row-outer-wrap
+        $this.addClass('SGPC_joinable');
+    });
 
 // Calculate which giveaways are joinable and which aren't
-checkJoinableGiveaways();
+    checkJoinableGiveaways();
 
 // Prevent default links events
-$("a.giveaway__heading__name").click(function (e) {
-    e.preventDefault();
-});
+    $("a.giveaway__heading__name").click(function (e) {
+        e.preventDefault();
+    });
 
 // When a giveaway is automatically joined recalculate which giveaways are joinable and which aren't
 // Also track points without needing to reload page
-$("a.giveaway__heading__name").mousedown(function (e) {
-    
-    /**
-     * ctrlKey -> CONTROL
-     * shiftKey -> SHIFT
-     * metaKey -> CMD (MAC)
-     * which = 1 -> Left Mouse Button
-     * which = 2 -> Middle Mouse Button
-     */
-    if (e.ctrlKey || e.shiftKey || e.metaKey || e.which === 1 || e.which === 2) {
-        var $parent = $(this).closest('div.giveaway__row-outer-wrap');
+    $("a.giveaway__heading__name").mousedown(function (e) {
 
-        if ($parent.hasClass('SGPC_joinable')) {
-            var totalpoints = $("span.nav__points").html();
-            var points = cleanPointsString($parent.find('span.giveaway__heading__thin').last().html());
+        /**
+         * ctrlKey -> CONTROL
+         * shiftKey -> SHIFT
+         * metaKey -> CMD (MAC)
+         * which = 1 -> Left Mouse Button
+         * which = 2 -> Middle Mouse Button
+         */
+        if (e.ctrlKey || e.shiftKey || e.metaKey || e.which === 1 || e.which === 2) {
+            var $parent = $(this).closest('div.giveaway__row-outer-wrap');
 
-            totalpoints -= points;
+            if ($parent.hasClass('SGPC_joinable')) {
+                var totalpoints = $("span.nav__points").html();
+                var points = cleanPointsString($parent.find('span.giveaway__heading__thin').last().html());
 
-            $("span.nav__points").html(totalpoints);
-            $parent.removeClass('SGPC_joinable');
-            $parent.addClass('is-faded');
+                totalpoints -= points;
 
-            checkJoinableGiveaways();
+                $("span.nav__points").html(totalpoints);
+                $parent.removeClass('SGPC_joinable');
+                $parent.addClass('is-faded');
+
+                checkJoinableGiveaways();
+            }
+
+            var openInBackground = true;
+            GM_openInTab($(this).attr("href"), openInBackground);
         }
-        
-        var openInBackground = true;
-        GM_openInTab($(this).attr("href"), openInBackground);
-    }
+    });
 });
